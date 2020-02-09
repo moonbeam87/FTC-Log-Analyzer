c8fd11f4f7e365296994f71451da70df287f70f1
moonbeam87 <2021dpatel@gmail.com>
2020-02-09 09:49:35 -0500

    updated folder

---
+++ Log Analyzer/app.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#By Dev Patel from FTC Team Robot Uprising 14607
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import overview as ov
+
+app = dash.Dash(
+    __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
+)
+server = app.server
+
+# Describe the layout/ UI of the app
+app.layout = html.Div(
+    [dcc.Location(id="url", refresh=False), html.Div(id="page-content")]
+)
+
+# Update page
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def display_page(pathname):
+    #Code for Sample Pages are required, would need a ".py" file similar to "overview.py"
+##    if pathname == "/ftc-log-analyzer/sample-page-1":
+##        return sample-page-1.create_layout(app)
+##    elif pathname == "/ftc-log-analyzer/sample-page-2":
+##        return sample-page-2.create_layout(app)
+##    else:
+    return ov.create_layout(app)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
